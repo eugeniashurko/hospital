cf67b2e4a60014b8cfb53a6696c3afc621d665ee
Oleksandra28 <oleksandrasopova@gmail.com>
2014-11-23 23:46:46 +0200

    Controller, EventDispatcher

--- HMS/Controller/EventDispatcher.py
+++ HMS/Controller/EventDispatcher.py
@@ -1,1 +1,14 @@
 __author__ = 'Саша'
+
+
+class EventDispatcher:
+
+    def __init__(self):
+        self.controllerContainer = []
+
+
+    def addController(self, controller):
+        pass
+
+    def removeController(self, controller):
+        pass
